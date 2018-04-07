ef2a547bda83d932a4f48346b2162737059bb0c7
alex.mititelu <blbla@yahoo.com>
2018-04-08 01:07:18 +0300

    Initial commit

---
+++ src/main/java/com/projectip/ProjectipApplication.java
@@ -0,0 +1,21 @@
+package com.projectip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ProjectipApplication {
+
+    @RequestMapping(value = "/greetings", method = RequestMethod.GET)
+    public String welcome() {
+        return "Greetings from B3!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectipApplication.class, args);
+    }
+}
