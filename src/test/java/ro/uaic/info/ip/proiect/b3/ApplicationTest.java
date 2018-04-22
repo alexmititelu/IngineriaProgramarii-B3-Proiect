c7897804949770c1a96abe3aebd9e32d4356dedf
gabrielxzc <codrincojocaru@yahoo.com>
2018-04-22 18:27:43 +0300

    Adaugat structura baza de date, restructurat main path

---
+++ src/test/java/ro/uaic/info/ip/proiect/b3/ApplicationTest.java
@@ -0,0 +1,17 @@
+package ro.uaic.info.ip.proiect.b3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void welcome() {
+
+    }
+
+    @Test
+    public void main() {
+
+    }
+}
