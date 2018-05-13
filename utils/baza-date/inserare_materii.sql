75a49efba9b65b4a120618a142059530c4989c02
Aurelia Grosu <grosu_aurelia20@yahoo.com>
2018-05-13 14:15:19 +0300

    Populare Baza Date

---
+++ utils/baza-date/inserare_materii.sql
@@ -0,0 +1,10 @@
+insert into materii values(1,'Ingineria Programarii');
+insert into materii values(2,'Programare Avansata');
+insert into materii values(3,'Matematica');
+insert into materii values(4,'PSGBD');
+insert into materii values(5,'Retele de Calculatoare');
+insert into materii values(6,'LFAC');
+insert into materii values(7,'Grafuri');
+
+
+commit;
