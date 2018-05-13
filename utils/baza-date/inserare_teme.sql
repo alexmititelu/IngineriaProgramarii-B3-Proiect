bcc5c969218bd244fd806a02ddb69b80636f7dda
Aurelia Grosu <grosu_aurelia20@yahoo.com>
2018-05-13 16:36:20 +0300

    Add files via upload

--- utils/baza-date/inserare_teme.sql
+++ utils/baza-date/inserare_teme.sql
@@ -1,10 +1,11 @@
-insert into teme values(1,10,4,'import_studenti.sql',STR_TO_DATE('21/03/2018', 'dd/mm/yyyy'));
-insert into teme values(2,15,2,'games.java',STR_TO_DATE('01/04/2018', 'dd/mm/yyyy'));
-insert into teme values(3,5,1,'inheritance.java',STR_TO_DATE('25/03/2018', 'dd/mm/yyyy'));
-insert into teme values(4,7,1,'mostenire.cpp',STR_TO_DATE('12/03/2018', 'dd/mm/yyyy'));
-insert into teme values(5,19,5,'file.c',STR_TO_DATE('21/10/2017', 'dd/mm/yyyy'));
-insert into teme values(6,6,1,'lab1.java',STR_TO_DATE('24/03/2018', 'dd/mm/yyyy'));
-insert into teme values(7,14,3,'SirulLuiRolle.m',STR_TO_DATE('13/04/2018', 'dd/mm/yyyy'));
-insert into teme values(8,17,7,'fisier.tex',STR_TO_DATE('24/11/2017', 'dd/mm/yyyy'));
+insert into teme values(1,4,SYSDATE(),'Creati un trigger care sa scrie intr-o tabela de tip LOG cate note au fost modificate de o comanda de tip update. Nu se va permite decat modificarea in plus a notelor.',1,'sql');
+insert into teme values(2,2,STR_TO_DATE('01/04/2018', 'dd/mm/yyyy'),'The Student-Project Allocation Problem (SPA) ',1,'java');
+insert into teme values(3,1,STR_TO_DATE('25/03/2018', 'dd/mm/yyyy'),'Realizati o aplicatie care va gestiona angajatii dintr-o firma.',1,'java');
+insert into teme values(5,5,STR_TO_DATE('21/10/2017', 'dd/mm/yyyy'),'Concurrent execution of two parent and child processes, using fork()',1,'c');
+insert into teme values(7,3,STR_TO_DATE('13/04/2018', 'dd/mm/yyyy'),'Realizati un script in care sa calculati cmmdc si cmmmc a doua numere naturale.',1,'m');
+insert into teme values(8,7,STR_TO_DATE('24/11/2017', 'dd/mm/yyyy'),'In graful G = (V, E) se considera 2 varfuri distincte
+¸si neadiacente s ¸si t. Exista ın G un drum colorat de la s la t ?',1,'tex');
+insert into teme values(4,6,STR_TO_DATE('20/11/2017', 'dd/mm/yyyy'),'Sa se afiseze cate aparitii ale cuvantului "infoiasi" se afla intr-un fisier.',1,'l');
+
 
 commit;
