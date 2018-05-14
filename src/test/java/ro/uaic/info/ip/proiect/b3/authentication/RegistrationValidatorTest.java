fb638a67be426894e1c51103e7e4a394c5bf3708
Cochior-Heghi Lucian <lucian.cochiorheghi@gmail.com>
2018-05-14 09:18:25 +0300

    Add JUnit test autentification/RegisterValidator

---
+++ src/test/java/ro/uaic/info/ip/proiect/b3/authentication/RegistrationValidatorTest.java
@@ -0,0 +1,29 @@
+package ro.uaic.info.ip.proiect.b3.authentication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegistrationValidatorTest {
+    RegistrationValidator registrationValidator = new RegistrationValidator();
+    @Test
+    public void isEmailValid() {
+
+        assertTrue(registrationValidator.isEmailValid("lucian.cochiorheghi@gmail.com"));
+    //    assertFalse(registrationValidator.isEmailValid("inexistentEmail"));
+    }
+
+    @Test
+    public void isUsernameValid() {
+        assertTrue(registrationValidator.isUsernameValid("testValidUser97"));
+        assertFalse(registrationValidator.isUsernameValid("flaviapopa"));
+        assertFalse(registrationValidator.isUsernameValid("rsad"));
+    }
+
+    @Test
+    public void isPasswordRespectingConstraints() {
+        assertTrue(registrationValidator.isPasswordRespectingConstraints("testpassword974A"));
+        assertFalse(registrationValidator.isPasswordRespectingConstraints("fdartg_@#$5fsd"));
+        assertFalse(registrationValidator.isPasswordRespectingConstraints("testp"));
+    }
+}
