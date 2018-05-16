698bb5940a799c477977da8ad689a84523a80bfd
procateodor <teoboy11@gmail.com>
2018-05-16 23:49:56 +0300

    add teme module

--- src/main/resources/static/assets/materie/scripts/main.js
+++ src/main/resources/static/assets/materie/scripts/main.js
@@ -60,6 +60,89 @@ $(document).ready(function () {
         }
     });
 
+    var add = document.getElementById('add');
+    var exercitii = 1;
+
+    add.onclick = event => {
+        event.preventDefault();
+        exercitii++;
+
+        var row = document.createElement('div');
+        row.classList.add('row');
+
+        var nameEx = document.createElement('div');
+        nameEx.classList.add('col-md-12');
+        nameEx.innerText = `Exercitiul ${exercitii}`;
+
+        var enunt = document.createElement('div');
+        enunt.classList.add('col-md-12');
+
+        var lbEnunt = document.createElement('label');
+        lbEnunt.innerText = 'Enunt';
+
+        var textarea = document.createElement('textarea');
+        textarea.classList.add('form-control');
+        textarea.classList.add('enunt-exercitiu');
+        textarea.setAttribute('cols', '30');
+        textarea.setAttribute('rows', '4');
+
+        enunt.appendChild(lbEnunt);
+        enunt.appendChild(textarea);
+
+        var extensie = document.createElement('div');
+        extensie.classList.add('col-md-4');
+
+        var grp = document.createElement('div');
+        grp.classList.add('form-group');
+
+        var lbExt = document.createElement('label');
+        lbExt.innerText = 'Extensie fisier acceptat';
+
+        var select = document.createElement('select');
+        select.classList.add('form-control');
+        select.classList.add('extensie');
+
+        var option1 = document.createElement('option');
+        option1.value = '';
+        option1.innerText = 'Extensie';
+        option1.setAttribute('selected', 'selected');
+        option1.setAttribute('disabled', 'disabled');
+
+        var option2 = document.createElement('option');
+        option2.value = 'cpp';
+        option2.innerText = 'C++';
+
+        var option3 = document.createElement('option');
+        option3.value = 'c';
+        option3.innerText = 'C';
+
+        var option4 = document.createElement('option');
+        option4.value = 'java';
+        option4.innerText = 'Java';
+
+        var option5 = document.createElement('option');
+        option5.value = 'py';
+        option5.innerText = 'Python';
+
+        select.appendChild(option1);
+        select.appendChild(option2);
+        select.appendChild(option3);
+        select.appendChild(option4);
+        select.appendChild(option5);
+
+        grp.appendChild(lbExt);
+        grp.appendChild(select);
+
+        extensie.appendChild(grp);
+
+        row.appendChild(nameEx);
+        row.appendChild(enunt);
+        row.appendChild(extensie);
+
+        var form = document.getElementById('form');
+
+        form.insertBefore(row, form.childNodes[form.childNodes.length - 6]);
+    }
 
 
     var form = document.getElementById('form');
@@ -71,29 +154,61 @@ $(document).ready(function () {
         var nume = document.getElementById('nume');
         var enunt = document.getElementById('enunt');
         var deadline = document.getElementById('deadline');
-        var nrExercitii = document.getElementById('nrExercitii');
-        var extensie = document.getElementById('extensie');
 
         var err = document.getElementById('err');
         var btn = document.getElementById('btn');
+
+        var add = document.getElementById('add');
+
+        var extesii = document.getElementsByClassName('extensie');
+
+        var extensiiTeme = [];
+
+        for (let index = 0; index < extesii.length; index++) {
+            const element = extesii[index];
+            extensiiTeme[index] = element.value;
+        }
+
+        var enunturi = document.getElementsByClassName('enunt-exercitiu');
+
+        var enunturiTeme = [];
+
+        for (let index = 0; index < enunturi.length; index++) {
+            const element = enunturi[index];
+            enunturiTeme[index] = element.value;
+        }
+
+        console.log(extensiiTeme, enunturiTeme);
 
         var data = {
             numeMaterie: numeMaterie,
             numeTema: nume.value,
             deadline: deadline.value,
             enunt: enunt.value,
-            nrExercitii: parseInt(nrExercitii.value),
-            extensieFisierAcceptat: extensie.value
+            nrExercitii: exercitii,
+            extensiiFisiere: extensiiTeme,
+            cerinteExercitii: enunturiTeme
         };
 
         err.innerHTML = '&nbsp;';
         nume.setAttribute('disabled', 'disabled');
         enunt.setAttribute('disabled', 'disabled');
         deadline.setAttribute('disabled', 'disabled');
-        nrExercitii.setAttribute('disabled', 'disabled');
-        extensie.setAttribute('disabled', 'disabled');
+        add.setAttribute('disabled', 'disabled');
         btn.setAttribute('disabled', 'disabled');
         btn.innerText = 'Se valideaza..';
+
+        for (let index = 0; index < enunturi.length; index++) {
+            const element = enunturi[index];
+            element.setAttribute('disabled', 'disabled');
+        }
+
+        for (let index = 0; index < extesii.length; index++) {
+            const element = extesii[index];
+            element.setAttribute('disabled', 'disabled');
+        }
+
+
 
         $.ajax({
             type: 'POST',
@@ -111,11 +226,20 @@ $(document).ready(function () {
                     err.classList.remove('success');
                     err.innerHTML = data;
 
+                    for (let index = 0; index < enunturi.length; index++) {
+                        const element = enunturi[index];
+                        element.setAttribute('disabled', 'disabled');
+                    }
+
+                    for (let index = 0; index < extesii.length; index++) {
+                        const element = extesii[index];
+                        element.setAttribute('disabled', 'disabled');
+                    }
+
                     nume.removeAttribute('disabled');
                     enunt.removeAttribute('disabled');
+                    add.removeAttribute('disabled');
                     deadline.removeAttribute('disabled');
-                    nrExercitii.removeAttribute('disabled');
-                    extensie.removeAttribute('disabled');
                     btn.removeAttribute('disabled');
                     btn.innerText = 'Creare materie';
                 }
