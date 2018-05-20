59746cdbf526db224889633e7f610ea18e4a2a2e
Mihalache_Stefan <mihalachestefan@yahoo.com>
2018-05-20 21:35:18 +0300

    ComparaController

--- src/main/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/ComparaController.java
+++ src/main/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/ComparaController.java
@@ -2,6 +2,12 @@ package ro.uaic.info.ip.proiect.b3.controllers.subject.homework;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import ro.uaic.info.ip.proiect.b3.database.Database;
+import ro.uaic.info.ip.proiect.b3.permissions.PermissionManager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Controller
 public class ComparaController {
@@ -13,6 +19,55 @@ public class ComparaController {
             @RequestParam("username1") String username1,
             @RequestParam("username2") String username2,
             @RequestParam("nrExercitiu") int nrExercitiu) {
-        return "";
+
+        Connection connection = null;
+        ResultSet user1;
+        ResultSet user2;
+        String nume1, prenume1 ,nume2, prenume2;
+
+        try{
+            if((!PermissionManager.isUserLoggedIn(loginToken))&&
+                    (PermissionManager.isLoggedUserProfesor(loginToken)) &&
+                    (PermissionManager.isUserAllowedToModifySubject(numeMaterie,loginToken))){
+
+                connection = Database.getInstance().getConnection();
+
+                user1 = Database.getInstance().selectQuery(connection, "select nume, prenume" +
+                        "  from studenti s join conturi c on s.email = c.email " +
+                        "where c.username = ?"
+                        , username1 );
+                while(user1.next()) {
+                    nume1 = user1.getString(1);
+                    prenume1 = user1.getString(2);
+                }
+
+                user2 = Database.getInstance().selectQuery(connection, "select nume, prenume" +
+                                "  from studenti s join conturi c on s.email = c.email " +
+                                "where c.username = ?"
+                        , username2 );
+                while(user2.next()) {
+                    nume2 = user2.getString(1);
+                    prenume2 = user2.getString(2);
+                }
+
+            }
+            else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.getMessage();
+            }
+        }
+
+        return "compara";
     }
 }
