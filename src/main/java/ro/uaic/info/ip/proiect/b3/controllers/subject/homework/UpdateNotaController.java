9466c6f4fe1807f58a1079151fc3d5cd7c744703
stanciusebastian <sebastian_s97@yahoo.ro>
2018-05-20 16:54:56 +0300

    Update nota controller

--- src/main/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/UpdateNotaController.java
+++ src/main/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/UpdateNotaController.java
@@ -1,19 +1,47 @@
-package ro.uaic.info.ip.proiect.b3.controllers.subject.homework;
+/*package ro.uaic.info.ip.proiect.b3.controllers.subject.homework;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import ro.uaic.info.ip.proiect.b3.database.Database;
+import ro.uaic.info.ip.proiect.b3.permissions.PermissionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Controller
 public class UpdateNotaController {
+
+    private final static Logger logger = Logger.getLogger(HomeworkController.class);
+
     @RequestMapping(value = "/materii/{numeMaterie}/{numeTema}/update_nota", method = RequestMethod.POST)
     public String updateNota(
             @CookieValue(value = "user", defaultValue = "-1") String loginToken,
+            @PathVariable("numeMaterie") String numeMaterie,
+            @PathVariable("numeMaterie") String nume_tema,
             @RequestParam("username") String usernameToGrade,
-            @RequestParam("nrExercitiu") int nrExercitiu,
-            @RequestParam("nota") int nota) {
-        return "valid";
+            @RequestParam("nrExercitiu") String nrExercitiu,
+            @RequestParam("nota") String nota) {
+        Connection connection = null;
+        try {
+            if (PermissionManager.isLoggedUserProfesor(loginToken) &&
+                    PermissionManager.isUserAllowedToModifySubject(numeMaterie, loginToken) &&
+                    PermissionManager.isRegisterTokenValid(loginToken)) {
+
+                Database.getInstance().updateOperation("update teme_incarcate set nota = ? where id_cont = " +
+                                "(select id from conturi where username=?) and numar exercitiu= ? and id_tema=" +
+                                "select id where nume_tema =?"
+                        , nota, usernameToGrade, nrExercitiu,nume_tema);
+
+
+                return "Valid";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "Invalid";
     }
 }
+*/
