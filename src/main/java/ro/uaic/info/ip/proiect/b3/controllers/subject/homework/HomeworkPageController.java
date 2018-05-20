66068f98f2240a1ea1d5d9eca787ac3e587cea33
SurduCosmin <cosmin.surdu@info.uaic.ro>
2018-05-20 13:37:27 +0300

    ^ finished HomeworkPageController

--- src/main/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/HomeworkPageController.java
+++ src/main/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/HomeworkPageController.java
@@ -3,13 +3,26 @@ package ro.uaic.info.ip.proiect.b3.controllers.subject.homework;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import ro.uaic.info.ip.proiect.b3.permissions.PermissionManager;
+import ro.uaic.info.ip.proiect.b3.permissions.PermissionManager.*;
+
+import java.sql.SQLException;
+
 
 @Controller
 public class HomeworkPageController {
     @RequestMapping(value = "/materii/{numeMaterie}/{numeTema}", method = RequestMethod.GET)
-    public String getHomeworkPage(@CookieValue(value = "user", defaultValue = "-1") String loginToken, Model model) {
-        return "";
+    public String getHomeworkPage(@CookieValue(value = "user", defaultValue = "-1") String loginToken) throws SQLException {
+        if (PermissionManager.isUserLoggedIn(loginToken)) {
+            return "./student/tema";
+        } else if (PermissionManager.isLoggedUserProfesor(loginToken)) {
+            return "./profesor/tema";
+        }
+        return "error";
     }
+
 }
