d01b35b7d29ce3548d21d9df7afb79565eb139f4
procateodor <teoboy11@gmail.com>
2018-05-21 16:35:39 +0300

    upload si solutie student

--- src/main/resources/static/assets/tema-studenti/scripts/main.js
+++ src/main/resources/static/assets/tema-studenti/scripts/main.js
@@ -12,10 +12,7 @@ $(document).ready(function () {
         url: `${window.location.href}/student_info_json`,
         success: data => {
             if (data.length > 0) {
-
-
                 data.forEach(element => {
-
                     exercitii++;
                     var a = document.createElement('a');
                     a.className = 'list-group-item list-group-item-action flex-column align-items-start';
@@ -50,24 +47,42 @@ $(document).ready(function () {
                     var row2 = document.createElement('div');
                     row2.className = 'row';
 
-                    var input = document.createElement('input');
-                    input.type = 'file';
-                    input.className = 'exFile';
+                    if (element.uploaded) {
+                        var btnValid = document.createElement('button');
+                        btnValid.className = 'btn btn-success solutie';
+                        btnValid.style = 'margin: 5px 0';
+                        btnValid.innerText = 'Vezi solutie';
+                        btnValid.name = exercitii;
+                        btnValid.setAttribute('role', 'button');
+                        btnValid.setAttribute('data-toggle', 'modal');
+                        btnValid.setAttribute('data-target', '#exampleModalCenter');
 
-                    row2.appendChild(input);
+                        row2.appendChild(btnValid);
 
-                    var row3 = document.createElement('div');
-                    row3.className = 'row';
+                        col1.appendChild(row2);
+                    } else {
+                        var input = document.createElement('input');
+                        input.type = 'file';
+                        input.className = 'exFile';
+                        input.name = exercitii;
+                        input.setAttribute('extensie', element.extensie);
 
-                    var btnUpload = document.createElement('button');
-                    btnUpload.className = 'btn btn-primary';
-                    btnUpload.style = 'margin: 5px 0';
-                    btnUpload.innerText = 'Incarca';
+                        row2.appendChild(input);
 
-                    row3.appendChild(btnUpload);
+                        var row3 = document.createElement('div');
+                        row3.className = 'row';
 
-                    col1.appendChild(row2);
-                    col1.appendChild(row3);
+                        var btnUpload = document.createElement('button');
+                        btnUpload.className = 'btn btn-primary upload';
+                        btnUpload.style = 'margin: 5px 0';
+                        btnUpload.innerText = 'Incarca';
+                        btnUpload.setAttribute('exercitiu', exercitii);
+
+                        row3.appendChild(btnUpload);
+
+                        col1.appendChild(row2);
+                        col1.appendChild(row3);
+                    }
 
                     row.appendChild(col1);
 
@@ -83,6 +98,102 @@ $(document).ready(function () {
 
                     group.appendChild(a);
                 });
+
+                var solutie = document.getElementsByClassName('solutie');
+                var table = document.getElementById('tableModel');
+
+                for (let index = 0; index < solutie.length; index++) {
+                    const element = solutie[index];
+
+                    element.onclick = () => {
+                        var line = 0;
+
+                        for (let index = 1; index < table.childNodes.length; index++) {
+                            const element = table.childNodes[index];
+
+                            table.removeChild(element);
+                        }
+
+                        $.ajax({
+                            type: 'POST',
+                            url: `${window.location.href}/continut_fisier`,
+                            data: {
+                                username: '-',
+                                nrExercitiu: parseInt(element.name)
+                            },
+                            success: data => {
+                                if (data.length > 0) {
+
+                                    data.forEach(element => {
+                                        line++;
+
+                                        var tr = document.createElement('tr');
+
+                                        var th = document.createElement('th');
+                                        th.innerText = line;
+
+                                        var td = document.createElement('td');
+                                        td.innerText = element;
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+
+                var upload = document.getElementsByClassName('upload');
+                var err = document.getElementById('err');
+
+                for (let index = 0; index < upload.length; index++) {
+                    const element = upload[index];
+
+                    element.onclick = () => {
+                        var input = element.parentNode.parentNode.childNodes[0].childNodes[0];
+                        err.innerText = '';
+                        err.innerHTML = '&nbsp;';
+
+                        if (input.files.length > 0) {
+                            var file = input.files[0];
+
+                            var nameFile = file.name;
+                            var extension = nameFile.split('.');
+
+                            var realExtension = extension[extension.length - 1];
+
+                            if (element.extensie !== realExtension) {
+                                err.innerText = 'Ai incarcat un fisier cu o extensie gresita.';
+                            } else {
+                                $.ajax({
+                                    type: 'POST',
+                                    url: `${window.location.href}/upload`,
+                                    data: {
+                                        nrExercitiu: element.exercitiu,
+                                        file: file
+                                    },
+                                    success: data => {
+                                        if (data === 'valid') {
+                                            element.classList.remove('btn-primary');
+                                            element.classList.add('btn-success');
+
+                                            element.innerText = 'Succes';
+
+                                            setTimeout(() => {
+                                                window.location.href = window.location.href;
+                                            }, 300);
+                                        } else {
+                                            element.classList.remove('btn-primary');
+                                            element.classList.add('btn-danger');
+
+                                            element.innerText = 'Incearca din nou';
+                                        }
+                                    }
+                                });
+                            }
+                        } else {
+                            err.innerText = 'Nu ai selectat niciun fisier pentru upload.';
+                        }
+                    }
+                }
             }
         }
     });
