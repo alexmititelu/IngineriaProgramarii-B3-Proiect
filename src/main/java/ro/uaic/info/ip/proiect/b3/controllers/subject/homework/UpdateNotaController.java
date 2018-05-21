d2ff4e28c0d5007eadc902ea21cbfcb40c6ef606
gabrielxzc <codrincojocaru@yahoo.com>
2018-05-21 23:23:09 +0300

    Update nota controller fixes

--- src/main/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/UpdateNotaController.java
+++ src/main/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/UpdateNotaController.java
@@ -5,34 +5,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ro.uaic.info.ip.proiect.b3.configurations.ServerErrorMessages;
 import ro.uaic.info.ip.proiect.b3.database.Database;
+import ro.uaic.info.ip.proiect.b3.database.objects.cont.Cont;
+import ro.uaic.info.ip.proiect.b3.database.objects.materie.Materie;
+import ro.uaic.info.ip.proiect.b3.database.objects.tema.Tema;
 import ro.uaic.info.ip.proiect.b3.permissions.PermissionManager;
 import java.sql.SQLException;
 
-
 @Controller
 public class UpdateNotaController {
-
     private final static Logger logger = Logger.getLogger(UpdateNotaController.class);
 
     @RequestMapping(value = "/materii/{numeMaterie}/{numeTema}/update_nota", method = RequestMethod.POST)
-    public String updateNota(
+    public @ResponseBody String updateNota(
             @CookieValue(value = "user", defaultValue = "-1") String loginToken,
             @PathVariable("numeMaterie") String numeMaterie,
-            @PathVariable("numeMaterie") String nume_tema,
+            @PathVariable("numeTema") String numeTema,
             @RequestParam("username") String usernameToGrade,
-            @RequestParam("nrExercitiu") String nrExercitiu,
-            @RequestParam("nota") String nota) {
-
+            @RequestParam("nrExercitiu") int nrExercitiu,
+            @RequestParam("nota") int nota) {
         try {
-            if (PermissionManager.isLoggedUserProfesor(loginToken) &&
-                    PermissionManager.isUserAllowedToModifySubject(numeMaterie, loginToken)) {
+            if (PermissionManager.isUserAllowedToModifySubject(numeMaterie, loginToken)) {
+                Materie materie = Materie.getByTitlu(numeMaterie);
+                if (materie == null) return "Materia cu acest nume nu exista!";
 
-                Database.getInstance().updateOperation("update teme_incarcate set nota = ? where id_cont = " +
-                                "(select id from conturi where username = ?) and nr_exercitiu = ? and id_tema = " +
-                                "(select id where nume_tema = ?)"
-                        , nota, usernameToGrade, nrExercitiu, nume_tema);
+                Tema tema = Tema.getByMaterieIdAndNumeTema(materie.getId(), numeTema);
+                if (tema == null) return "Tema cu acest nume din cadrul materiei nu exista!";
 
-                return "Valid";
+                Cont cont = Cont.getByUsername(usernameToGrade);
+                if (cont == null) return "Numele de utilizator pentru care se incearca editarea notei nu exista!";
+
+                if (nota < 1 || nota > 10) return "Nota poate lua valori doar intre 1 si 10!";
+
+                int updatedRows = Database.getInstance().updateOperation("update teme_incarcate set nota = ? where id_cont = ? and nr_exercitiu = ? and id_tema = ?",
+                        Integer.toString(nota), Long.toString(cont.getId()), Integer.toString(nrExercitiu), Long.toString(tema.getId()));
+
+                if (updatedRows == 0) return "Numarul exercitiului nu este valid!";
+
+                return "valid";
             } else {
                 return ServerErrorMessages.UNAUTHORIZED_ACCESS_MESSAGE;
             }
