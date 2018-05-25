2edc12f3031641e360f14e5d06c2d367f0b54681
petcuvlad <petcuvlad92@gmail.com>
2018-05-26 00:39:25 +0300

    ContinutFisierControllerTest

---
+++ src/test/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/ContinutFisierControllerTest.java
@@ -0,0 +1,51 @@
+package ro.uaic.info.ip.proiect.b3.controllers.subject.homework;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class ContinutFisierControllerTest {
+
+    private ContinutFisierController continutFisierController;
+    private String logTokenProf = "OFCNsb0EPavOggQ6pCFmhldNyTTnf2A8CGAgUXFUt8pn5zksto04eyXbKgSeiXil";
+    private String logTokenStud = "cnJ1GP1f92l1S8dvlOzDk9hwULhkMXERsMFDvJt71PZDRvc0zdymOMgNhoU1Drh0";
+    private String logTokenInvalid = ";bla";
+    private ArrayList<String>  continut;
+
+    @Before
+    public void setUp() throws Exception {
+        continutFisierController = new ContinutFisierController();
+        continut = new ArrayList<>();
+    }
+
+    @Test
+    public void getContinutFisier() {
+
+        assertEquals(null,continutFisierController
+                .getContinutFisier(logTokenInvalid,"testTeme","testTema1",1,"profesor"));
+
+        assertEquals(null,continutFisierController
+                .getContinutFisier(logTokenProf,";","testTema1",1,"profesor"));
+
+        assertEquals(null,continutFisierController
+                .getContinutFisier(logTokenProf,"testTeme",";",1,"profesor"));
+
+        assertEquals(null,continutFisierController
+                .getContinutFisier(logTokenProf,"testTeme","testTema1",-1,"profesor"));
+
+        assertEquals(null,continutFisierController
+                .getContinutFisier(logTokenStud,"testTeme","testTema1",-1,"vladpetcu"));
+
+        assertTrue("success",continut.isEmpty());
+
+        continut = continutFisierController
+                .getContinutFisier(logTokenStud,"test44","test",1,"vladpetcu");
+
+        assertFalse("success",continut.isEmpty());
+        assertEquals(273,continut.size());
+
+    }
+}
