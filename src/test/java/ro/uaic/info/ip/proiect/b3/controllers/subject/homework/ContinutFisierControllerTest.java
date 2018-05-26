b0b5f4c1445b847f30469b6ee7d065f4b0251458
petcuvlad <petcuvlad92@gmail.com>
2018-05-26 17:06:16 +0300

    ContinutFisierControllerTest

--- src/test/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/ContinutFisierControllerTest.java
+++ src/test/java/ro/uaic/info/ip/proiect/b3/controllers/subject/homework/ContinutFisierControllerTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class ContinutFisierControllerTest {
 
     private ContinutFisierController continutFisierController;
-    private String logTokenProf = "OFCNsb0EPavOggQ6pCFmhldNyTTnf2A8CGAgUXFUt8pn5zksto04eyXbKgSeiXil";
+    private String logTokenProf = "jmA9KxVmkXLO5RO4VA1eLokwzclGNogYlxC1Irh6nap4tHbYeOwkZ50evOsXyhk5";
     private String logTokenStud = "cnJ1GP1f92l1S8dvlOzDk9hwULhkMXERsMFDvJt71PZDRvc0zdymOMgNhoU1Drh0";
     private String logTokenInvalid = ";bla";
     private ArrayList<LinieContinutFisier>  continut;
@@ -42,7 +42,8 @@ public class ContinutFisierControllerTest {
         continut = continutFisierController
                 .getContinutFisier(logTokenStud,"test44","test",1,"vladpetcu");
 
-//        assertFalse("success",continut.isEmpty());
+        assertFalse("success",continut.isEmpty());
+        assertEquals(273,continut.size());
 
 
 
