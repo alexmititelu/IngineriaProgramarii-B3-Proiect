6e57836ca84f0d96e407fb6fd5111d1b6e13dfb1
gabrielxzc <codrincojocaru@yahoo.com>
2018-06-01 16:11:23 +0300

    Modificari pagina home-no-logged

--- src/main/resources/static/assets/home-no-logged/scripts/main.js
+++ src/main/resources/static/assets/home-no-logged/scripts/main.js
@@ -151,4 +151,44 @@ $(document).ready(function () {
             }
         });
     }
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            let navbar = document.getElementsByClassName("nav-fixed")[0];
+
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    console.log(navbar.offsetHeight);
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - navbar.offsetHeight
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
 });
